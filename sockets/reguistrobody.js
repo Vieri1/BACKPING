6e1dc0264d16897eb5898dc81cdb98fd218215c6
vieri1 <vieriflores2002xdxd@gmail.com>
2025-02-05 05:13:41 -0500

    waa tengo señoo

--- sockets/reguistrobody.js
+++ sockets/reguistrobody.js
@@ -1,45 +1,90 @@
 const { getIo } = require('../sockets');
-const {newbody}=require("../controllers/bodyCamController")
-// Usas getIo para obtener la instancia de io
-const io = getIo();
+const { newbody, updatebodyCam } = require("../controllers/bodyCamController");
 
-// Ahora puedes emitir eventos
-io.on('reguistro', async ({numero,serie,nro_bateria})=>{
-    const errores=[]
-    const regexNumeros = /^[0-9]+$/;
-    const regexLetrasNumeros = /^[a-zA-Z0-9]+$/;
-    if(!numero){
-        errores.push("El campo numero es requerido")
-    } else if (!regexNumeros.test(numero)) {
-        errores.push("El campo 'numero' solo debe contener números.");
-    }
-    if(!serie){
-        errores.push("El campo serie es requerido")
-    }else if(!regexLetrasNumeros.test(serie)){
-        errores.push("La serie solo debe tener numero y letras ")
-    }
-    if(!nro_bateria){
-        errores.push("El campo nro_bateria es requerido")
-    }else if(!regexLetrasNumeros.test(nro_bateria)){
-        errores.push("El nro_bateria solo debe tener numero y letras ")
-    }
-    if(errores.length>0){
-        io.emit("errorRegistro",{errores})
-    }
-    try {
-        // Intentamos registrar la nueva bodyCam
-        const newBody = await newbody({ numero, serie, nro_bateria });
+const socketHandlers = (socket) => {
+   // console.log(`Cliente conectado: ${socket.id}`);
 
-        if (!newBody) {
-            return io.emit("errorRegistro", {
-                errores: ["Error al registrar la bodyCam."],
-            });
+    // Evento para actualizar BodyCam
+    socket.on('updateBody', async (data, callback) => {
+        const { id, numero, serie, nro_bateria, id_proveedor } = data;
+
+        const errores = [];
+        const regexNumeros = /^[0-9]+$/;
+        const regexLetrasNumeros = /^[a-zA-Z0-9]+$/;
+
+        if (!numero) errores.push("El campo numero es requerido");
+        else if (!regexNumeros.test(numero)) errores.push("El campo 'numero' solo debe contener números.");
+
+        if (!serie) errores.push("El campo serie es requerido");
+        else if (!regexLetrasNumeros.test(serie)) errores.push("La serie solo debe tener números y letras.");
+
+        if (!nro_bateria) errores.push("El campo nro_bateria es requerido");
+        else if (!regexLetrasNumeros.test(nro_bateria)) errores.push("El nro_bateria solo debe tener números y letras.");
+
+        if (errores.length > 0) {
+            return callback({ status: 400, errores });
         }
 
-        // Si se registra correctamente, notificamos a todos los clientes
-        io.emit("registroExitoso", { message: "BodyCam registrada con éxito", data: newBody });
-    } catch (error) {
-        console.error("Error al registrar BodyCam en el socket:", error);
-        io.emit("errorRegistro", { errores: ["Error interno del servidor."] });
-    }
-});
+        try {
+            const response = await updatebodyCam(id, { numero, serie, nro_bateria, id_proveedor });
+
+            if (!response) {
+                return callback({ status: 500, message: "Error al modificar la BodyCam." });
+            }
+
+            // Emitir evento a todos los clientes
+            const io = getIo();
+            io.emit("bodyCamActualizada", { message: "BodyCam modificada con éxito", data: response });
+
+            callback({ status: 200, message: "BodyCam modificada con éxito", data: response });
+        } catch (error) {
+            console.error("Error al modificar BodyCam:", error);
+            callback({ status: 500, message: "Error interno del servidor" });
+        }
+    });
+
+    // Evento para crear una nueva BodyCam
+    socket.on('createBody', async (data, callback) => {
+        const { numero, serie, nro_bateria } = data;
+
+        const errores = [];
+        const regexNumeros = /^[0-9]+$/;
+        const regexLetrasNumeros = /^[a-zA-Z0-9]+$/;
+
+        if (!numero) errores.push("El campo numero es requerido");
+        else if (!regexNumeros.test(numero)) errores.push("El campo 'numero' solo debe contener números.");
+
+        if (!serie) errores.push("El campo serie es requerido");
+        else if (!regexLetrasNumeros.test(serie)) errores.push("La serie solo debe tener números y letras.");
+
+        if (!nro_bateria) errores.push("El campo nro_bateria es requerido");
+        else if (!regexLetrasNumeros.test(nro_bateria)) errores.push("El nro_bateria solo debe tener números y letras.");
+
+        if (errores.length > 0) {
+            return callback({ status: 400, errores });
+        }
+
+        try {
+            const newBody = await newbody({ numero, serie, nro_bateria });
+
+            if (!newBody) {
+                return callback({ status: 500, message: "Error al registrar la BodyCam." });
+            }
+
+            // Emitir evento a todos los clientes
+            const io = getIo();
+            io.emit("bodyCamRegistrada", { message: "BodyCam registrada con éxito", data: newBody });
+
+            callback({ status: 200, message: "BodyCam registrada con éxito", data: newBody });
+        } catch (error) {
+            console.error("Error al registrar BodyCam:", error);
+            callback({ status: 500, message: "Error interno del servidor" });
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`Cliente desconectado: ${socket.id}`);
+    });
+};
+
+module.exports = socketHandlers;
